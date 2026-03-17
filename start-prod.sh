0d7018add25ccec01e7737431949b7ae9e01baf6
hardijs kivers <hardijs.kivers@whitedigital.eu>
2026-03-17 17:34:36 +0200

    lobby bots

--- start-prod.sh
+++ start-prod.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
+if [ "$1" = "--pull" ] || [ "$1" = "-p" ]; then
+  git pull
+fi
+docker compose --env-file docker/prod/.env -f docker/prod/docker-compose.yml build --no-cache draft-lobbybot
 docker compose --env-file docker/prod/.env -f docker/prod/docker-compose.yml up -d --build
